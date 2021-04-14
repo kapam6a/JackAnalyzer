c2467af3e0bc570a2310b7c8592bb0efb4a53e46
Алексей Якименко <ayakimenko@Aleksejs-MacBook-Air.local>
2021-04-14 16:15:55 +0700

    Added compilation engine

---
+++ JackAnalyzer/CompilationEngine.swift
@@ -0,0 +1,78 @@
+//
+//  CompilationEngine.swift
+//  JackAnalyzer
+//
+//  Created by Алексей Якименко on 14.04.2021.
+//
+
+import Foundation
+
+final class CompilationEngine {
+    
+    private let tokenizer: Tokenizer
+    
+    init(_ tokenizer: Tokenizer) {
+        self.tokenizer = tokenizer
+    }
+    
+    func compileClass() {
+        
+    }
+    
+    func compileClassVarDec() {
+        
+    }
+    
+    func compileSubroutineDec() {
+        
+    }
+    
+    func compileParameterList() {
+        
+    }
+    
+    func compileSubroutineBody() {
+        
+    }
+    
+    func compileVarDec() {
+        
+    }
+    
+    func compileStatements() {
+        
+    }
+    
+    func compileLet() {
+        
+    }
+    
+    func compileIf() {
+        
+    }
+    
+    func compileWhile() {
+        
+    }
+    
+    func compileDo() {
+        
+    }
+    
+    func compileReturn() {
+        
+    }
+    
+    func compileExpression() {
+        
+    }
+    
+    func compileTerm() {
+        
+    }
+    
+    func compileExpressionList() {
+        
+    }
+}
+
